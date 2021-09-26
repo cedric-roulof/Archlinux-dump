fb15d347fb899a23b43293898a7ade80bda09a58
kevin-cazal <50819904+kevin-cazal@users.noreply.github.com>
2021-09-26 09:54:56 -0400

    Fedora 34

--- install_packages_dump.sh
+++ install_packages_dump.sh
@@ -6,9 +6,9 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-cat /etc/fedora-release | grep "Fedora release 32"
+cat /etc/fedora-release | grep "Fedora release 34"
 if [[ $? -ne 0 ]]; then
-    echo "This script must be run onto a Fedora 32";
+    echo "This script must be run onto a Fedora 34";
     exit 1
 fi
 echo "Press ENTER to continue..."
